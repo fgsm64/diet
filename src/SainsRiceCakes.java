0ff485798fd82bf27084e77b0effb54c5d57136b
fgsm64 <123595266+fgsm64@users.noreply.github.com>
2026-03-28 13:12:09 +0000

    refactor(food): refactor SainsRiceCakes inline with food refactor

--- src/SainsRiceCakes.java
+++ src/SainsRiceCakes.java
@@ -1,42 +1,21 @@
 public class SainsRiceCakes extends Food {
 
+    protected void initNutrition() {
+        this.fatPer100 = 2.7;
+        this.satFatPer100 = 0.7;
+        this.carbPer100 = 82.3;
+        this.sugarPer100 = 0.5;
+        this.proteinPer100 = 8.5;
+        this.saltPer100 = 0.3;
+        this.calsPer100 = 394;
+    }
+
     public SainsRiceCakes(double mass, double price) {
         super(mass, price);
+        initNutrition();
     }
 
-    
-    public double getFatPer100Grams() {
-        return 2.7;
+    public SainsRiceCakes(double mass) {
+        this(mass, 0);
     }
-
-    
-    public double getSatFatPer100Grams() {
-        return 0.7;
-    }
-
-    
-    public double getCarbPer100Grams() {
-        return 82.3;
-    }
-
-    
-    public double getSugarPer100Grams() {
-        return 0.5;
-    }
-
-    
-    public double getProteinPer100Grams() {
-        return 8.5;
-    }
-    
-    
-    public double getSaltPer100Grams() {
-        return 0.3;
-    }
-
-    
-    public double getCalsPer100Grams() {
-        return 394;
-    }
-
 }
